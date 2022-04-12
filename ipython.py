4756bec79949edd0be6ce4a357faa9b9b5da4ec7
chillaranand <anand21nanda@gmail.com>
2022-04-12 10:49:25 +0530

    fix: Code cleanup

--- ipython.py
+++ ipython.py
@@ -3,51 +3,46 @@ import sys
 
 import frappe
 import requests
-# from erpnext.setup.setup_wizard.utils import complete
 from frappe.utils.install import complete_setup_wizard
-from frappe.desk.page.setup_wizard.setup_wizard import setup_complete
 from frappe.desk.page.setup_wizard.setup_wizard import make_records
 
 
 print('Setup Wizard')
-# setup_complete()
 complete_setup_wizard()
 
 
 def fetch_records():
-        try:
-                json_file = sys.argv[1]
-        except:
-                json_file = '/tmp/records.json'
-                json_url = 'https://raw.githubusercontent.com/ChillarAnand/frappe_init/main/records.json'
-                response = requests.get(json_url)
-                with open(json_file, 'w') as fh:
-                        fh.write(response.text)
+    try:
+        json_file = sys.argv[1]
+    except:
+        json_file = '/tmp/records.json'
+        json_url = 'https://raw.githubusercontent.com/ChillarAnand/frappe_init/main/records.json'
+        response = requests.get(json_url)
+        with open(json_file, 'w') as fh:
+            fh.write(response.text)
 
-        records = json.loads(open(json_file).read())
-        return records
+    records = json.loads(open(json_file).read())
+    return records
 
 
 def create_records(records):
-        for record in records:
+    for record in records:
+        frappe.db.commit()
+        try:
+            if record.get('name'):
+                exists = frappe.db.exists(record['doctype'], record['name'])
+            else:
+                exists = frappe.db.exists(record)
+            if not exists:
+                print('Creating ' + record['doctype'])
+                make_records([record])
                 frappe.db.commit()
-                try:
-                        if record.get('name'):
-                                exists = frappe.db.exists(record['doctype'], record['name'])
-                        else:
-                                exists = frappe.db.exists(record)
-                        if not exists:
-                                print('Creating ' + record['doctype'])
-                                make_records([record])
-                                frappe.db.commit()
-                        else:
-                                print('Skipping ' + record['doctype'])
-                except ValueError as e:
-                        frappe.db.rollback()
-                        print('Failed ' + record['doctype'])
-                        print(str(e))
-                        # print('Failed ' + str(e))
-
+            else:
+                print('Skipping ' + record['doctype'])
+        except Exception as e:
+            frappe.db.rollback()
+            print('Failed ' + record['doctype'])
+            print(str(e))
 
 
 records = fetch_records()
