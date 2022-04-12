bd9da78e8eef9bc87ca3cb323ea046e01b70ab77
chillaranand <anand21nanda@gmail.com>
2022-04-12 10:48:31 +0530

    fix: Fix issue with holiday lists

--- ipython.py
+++ ipython.py
@@ -15,35 +15,38 @@ complete_setup_wizard()
 
 
 def fetch_records():
-	try:
-		json_file = sys.argv[1]
-	except:
-		json_file = '/tmp/records.json'
-		json_url = 'https://raw.githubusercontent.com/ChillarAnand/frappe_init/main/records.json'
-		response = requests.get(json_url)
-		with open(json_file, 'w') as fh:
-			fh.write(response.text)
+        try:
+                json_file = sys.argv[1]
+        except:
+                json_file = '/tmp/records.json'
+                json_url = 'https://raw.githubusercontent.com/ChillarAnand/frappe_init/main/records.json'
+                response = requests.get(json_url)
+                with open(json_file, 'w') as fh:
+                        fh.write(response.text)
 
-	records = json.loads(open(json_file).read())
-	return records
+        records = json.loads(open(json_file).read())
+        return records
 
 
 def create_records(records):
-	for record in records:
-		frappe.db.commit()
-		try:
-			exists = frappe.db.exists(record)
-			if not exists:
-				print('Creating ' + record['doctype'])
-				make_records([record])
-				frappe.db.commit()
-			else:
-				print('Skipping ' + record['doctype'])
-		except Exception as e:
-			frappe.db.rollback()
-			print('Failed ' + record['doctype'])
-			print(str(e))
-			# print('Failed ' + str(e))
+        for record in records:
+                frappe.db.commit()
+                try:
+                        if record.get('name'):
+                                exists = frappe.db.exists(record['doctype'], record['name'])
+                        else:
+                                exists = frappe.db.exists(record)
+                        if not exists:
+                                print('Creating ' + record['doctype'])
+                                make_records([record])
+                                frappe.db.commit()
+                        else:
+                                print('Skipping ' + record['doctype'])
+                except ValueError as e:
+                        frappe.db.rollback()
+                        print('Failed ' + record['doctype'])
+                        print(str(e))
+                        # print('Failed ' + str(e))
 
 
 
